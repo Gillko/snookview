8c9f67ffab05f7aa5b7d046fca7da7aff8d7eddd
Gilko <gilles_vanpeteghem@hotmail.com>
2017-08-25 20:27:04 +0200

    20th Commit - adding a video to the routes files

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -538,6 +538,7 @@
 	Router::connect("/videos/the-masters-2015/first-round/stuart-bingham-vs-marco-fu", array("controller" => "videos", "action" => "view", 269));
 	Router::connect("/videos/the-masters-2015/first-round/stuart-bingham-vs-marco-fu-highlights", array("controller" => "videos", "action" => "view", 270));
 	Router::connect("/videos/the-masters-2015/first-round/judd-trump-vs-stephen-maguire", array("controller" => "videos", "action" => "view", 271));
+	Router::connect("/videos/the-masters-2015/first-round/neil-robertson-vs-robert-milkins", array("controller" => "videos", "action" => "view", 272));
 
 	Router::connect('/videos/*', array('controller' => 'videos', 'action' => 'view'));
 
